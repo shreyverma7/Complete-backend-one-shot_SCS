6a3c23f0b13af01e32676fc417f1ddb4c43c7856
shreyverma7 <shrey007verma@gmail.com>
2026-02-10 14:52:12 +0530

    clear the commands

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 
-const app = express();  //create an server using express
-
+const app = express();  
 app.get('/', (req, res) => {
-  res.send('Hello, World!'); //send a response to the client when the root URL is accessed
+  res.send('Hello, World!');
 });
 
 app.get('/about', (req, res) => {
-  res.send('This is the about page.'); //send a response to the client when the /about URL is accessed
+  res.send('This is the about page.');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}); //start the server and listen on port 3000
+app.listen(3000); 
