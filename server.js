010ca205b473b293f1187f049e80d476e0de26c1
shreyverma7 <shrey007verma@gmail.com>
2026-02-10 14:47:27 +0530

    how to create server and Set respond

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
 
-express();
+const app = express();  //create an server using express
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!'); //send a response to the client when the root URL is accessed
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is the about page.'); //send a response to the client when the /about URL is accessed
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}); //start the server and listen on port 3000
