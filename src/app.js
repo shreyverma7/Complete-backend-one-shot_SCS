b74bb60497b2601f7bd185225fcb270ef70f24c2
shreyverma7 <shrey007verma@gmail.com>
2026-02-11 15:13:51 +0530

    Middle ware and try to do post using postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,26 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json()); //Middle  to parse JSON request body
+
+
+const notes = [];
+app.post('/notes', (req, res) => {
+  
+  console.log(req.body);
+  res.status(201).json({ message: 'Note created successfully' });
+});
+
+//? Format fo notes 
+/* const notes = [
+  {
+    title: "my first note",
+    description: "this is my first note",
+  },
+
+  { title: "my second note", description: "this is my second note" },
+]; */
+
 
 module.exports = app;
 
