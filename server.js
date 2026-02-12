954bb4d602967de501754a2186748912b055ce34
shreyverma7 <shrey007verma@gmail.com>
2026-02-12 16:21:57 +0530

    Connecction with DB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//start server using express is server.js works
-
 const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
+connectDB();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
---
+++ src/db/db.js
@@ -0,0 +1,11 @@
+const moongoose = require("mongoose");
+
+
+async function connectDB() {
+    await moongoose.connect("mongodb+srv://CompleteBackendYT_01:P2WjxE60rtjdRZvQ@atlascluster.hoysxft.mongodb.net/BackendOneShortDB")
+
+    console.log("Connected to MongoDB");
+}
+
+module.exports = connectDB;
+
