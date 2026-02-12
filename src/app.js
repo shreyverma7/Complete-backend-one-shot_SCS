cc8905458d17ae9cfb85ef5d40bd9e485e982bf3
shreyverma7 <shrey007verma@gmail.com>
2026-02-13 00:01:07 +0530

    Completed CRUD operation with RestFul api's

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const noteModel = require("./models/note.model");
 
 const app = express();
-app.use(express.json()); // This middleware is used to parse the JSON data sent in the request body. It allows us to access the data using req.body in our route handlers.
+app.use(express.json());
 
 /*
  POST /notes - Create a new note
@@ -11,13 +11,43 @@ DELETE /notes/:id - Delete a note by ID
 PATCH /notes/:id - Update a note by ID
  */
 
-app.post("/notes", async(req, res) => {
+app.post("/notes", async (req, res) => {
   const data = req.body; /* {title , description} */
   await noteModel.create({
     title: data.title,
     description: data.description,
   });
-  res.status(201).json({ message: "Note created successfully" });
+  res.status(201).json({ message: "Note created successfully", data: data });
+});
+
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find(); // [] or [{title, description}]
+  //const notes = await noteModel.find({},{title: "test_title3" }); // [] or [{title}] find only title which is "test_title3"
+  //const notes = await noteModel.find({title: "test_title3" }); // [] or [{title}] find only title which is "test_title3"
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    data: notes,
+  });
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findOneAndDelete({
+    _id: id,
+  });
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  //   const data = req.body; /* {title , description} */
+  const description = req.body.description;
+  await noteModel.findOneAndUpdate({ _id: id }, { description: description });
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
 });
 
 module.exports = app;
