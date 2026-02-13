ea04aab2d835bc60b79aacbd05f9892ffab4b6c2
shreyverma7 <shrey007verma@gmail.com>
2026-02-14 00:37:23 +0530

    get api also completd

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ const postModel = require("./models/post.model");
 const app = express();
 app.use(express.json());
 
-const upload = multer({ storage: multer.memoryStorage() }); // Store uploaded files in memory
-//const upload = multer({ storage: 'uploads/' }); // Store uploaded files in the 'uploads' directory
+const upload = multer({ storage: multer.memoryStorage() }); 
 
 app.post("/create-post", upload.single("image"), async (req, res) => {
-  //    console.log(req.body);
-  //    console.log(req.file);
-
   const result = await uploadFile(req.file.buffer);
-  // console.log(result);
   const post = await postModel.create({
     image: result.url,
     caption: req.body.caption,
   });
   return res.status(201).json({ message: "Post created successfully", post });
+});
+
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
+  return res.status(200).json({ message: "Posts retrieved successfully", posts });
 });
 
 module.exports = app;
--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,15 +1,11 @@
 
-const { ImageKit } = require("@imagekit/nodejs");     // For CommonJS older version of Node.js
-// import ImageKit from '@imagekit/nodejs';  // For ES module newer version of Node.js
-
-
+const { ImageKit } = require("@imagekit/nodejs");    
 
 const imagekit = new ImageKit({
      privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function uploadFile(buffer) {
-    // console.log(buffer);
     const result = await imagekit.files.upload({
         file : buffer.toString("base64"),
         fileName : "image.jpg",
