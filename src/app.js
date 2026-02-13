65527dd9ac29c0377177d09ab229ff71a68dff74
shreyverma7 <shrey007verma@gmail.com>
2026-02-13 20:22:19 +0530

    Creating a new project which take image and caption like instagram

--- src/app.js
+++ src/app.js
@@ -11,43 +11,10 @@ DELETE /notes/:id - Delete a note by ID
 PATCH /notes/:id - Update a note by ID
  */
 
-app.post("/notes", async (req, res) => {
-  const data = req.body; /* {title , description} */
-  await noteModel.create({
-    title: data.title,
-    description: data.description,
-  });
-  res.status(201).json({ message: "Note created successfully", data: data });
+app.post("/create-post", async (req, res) => {
+   console.log(req.body);
 });
 
-app.get("/notes", async (req, res) => {
-  const notes = await noteModel.find(); // [] or [{title, description}]
-  //const notes = await noteModel.find({},{title: "test_title3" }); // [] or [{title}] find only title which is "test_title3"
-  //const notes = await noteModel.find({title: "test_title3" }); // [] or [{title}] find only title which is "test_title3"
-  res.status(200).json({
-    message: "Notes fetched successfully",
-    data: notes,
-  });
-});
 
-app.delete("/notes/:id", async (req, res) => {
-  const id = req.params.id;
-  await noteModel.findOneAndDelete({
-    _id: id,
-  });
-  res.status(200).json({
-    message: "Note deleted successfully",
-  });
-});
-
-app.patch("/notes/:id", async (req, res) => {
-  const id = req.params.id;
-  //   const data = req.body; /* {title , description} */
-  const description = req.body.description;
-  await noteModel.findOneAndUpdate({ _id: id }, { description: description });
-  res.status(200).json({
-    message: "Note updated successfully",
-  });
-});
 
 module.exports = app;
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const moongoose = require("mongoose");
 
 
 async function connectDB() {
-    await moongoose.connect("mongodb+srv://CompleteBackendYT_01:P2WjxE60rtjdRZvQ@atlascluster.hoysxft.mongodb.net/BackendOneShortDB")
+    await moongoose.connect("mongodb+srv://CompleteBackendYT_01:P2WjxE60rtjdRZvQ@atlascluster.hoysxft.mongodb.net/Project_01");
     console.log("Connected to MongoDB");
 }
 
